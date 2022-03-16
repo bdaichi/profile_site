5280c298ad6308e0d228e604ad9a7abdaf3a492b
別府大地 <beppudaichi@beppudaichis-MacBook-Air.local>
2022-03-16 16:47:47 +0900

    portfolio_pageのデザインが決まった

--- pages/portfolio_page.tsx
+++ pages/portfolio_page.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import Portfolio from "../component/portfolio_page/portfolio";
 
 export default function PortfolioPage() {
     const router = useRouter()
@@ -6,10 +7,12 @@ export default function PortfolioPage() {
     const query = router.query;
     const portfolioName = query.portfolioName as string;
 
+    const storeListSummary: string = '買い物用のアプリです'
+
     return (
         <>
             <div>{portfolioName == 'S t o r e L i s t' &&
-                <p>1</p>
+                <Portfolio portfolioName='Store List' imagePath='storeList.jpg' summary={storeListSummary} />
             }</div>
             <div>{portfolioName == 'StoreList' &&
                 <p>2</p>
