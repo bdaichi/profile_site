42b4696b6fd3c8e358f3b7445fa3f35053f08110
別府大地 <beppudaichi@beppudaichis-MacBook-Air.local>
2022-03-17 12:01:26 +0900

    プロフィールができた

--- component/home_page/profile_feild.tsx
+++ component/home_page/profile_feild.tsx
@@ -1,11 +1,23 @@
+import { Button } from "@mui/material";
+import { useRouter } from "next/router";
+
 export default function ProfileFeild() {
+    const router = useRouter()
+
+    const routerProfilePage = () => {
+        router.push('/profile_page')
+    }
 
     return (
         <>
-            <div className='fixed top-32 md:top-44 md:left-8 md:w-56 p-8 shadow-2xl rounded-md bg-white z-20 flex flex-col justify-center items-center m-4'>
-                <img className='m-4 h-56 md:h-full rounded-2xl' src='beppudaichi.jpg' />
-                <p className='text-3xl tracking-widest my-4'>別府大地</p>
-            </div>
+            <Button
+                onClick={routerProfilePage}
+            >
+                <div className='fixed top-32 md:top-44 md:left-8 md:w-56 p-8 md:shadow-2xl rounded-md bg-white z-20 flex flex-col justify-center items-center m-4'>
+                    <img className='m-4 h-56 md:h-full rounded-2xl' src='beppudaichi.jpg' />
+                    <p className='text-3xl tracking-widest my-4'>別府大地</p>
+                </div>
+            </Button>
         </>
     )
 }
